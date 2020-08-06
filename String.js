78e9ba5f3f7966dedbe9b55c265b8da82904d428
gun <thiraphat.ps.dev@gmail.com>
2020-08-06 22:57:44 +0700

    update

--- String.js
+++ String.js
@@ -8,26 +8,46 @@ const strSingle = 'Hello world';
 // String can also be treated like zero index based character array.
 const str3 = 'Hello World';
 
-str3.length //  11
+str3.length 
+// expected output: 11
 
 // String is character index, it can be accessed using for loop
 const str4 = 'Hello World';
 
 for (let i = 0; i < str4.length; i++) {
-//    console.log(str4[i])
+  console.log(str4[i])
 }
+// expected output: 
+// H
+// e
+// l 
+// l 
+// o 
+// 
+// w
+// o
+// r
+// l
+// d
 
 // Concatenation
 const str5 = 'Hello ' + "World " + 'from ' + 'TutorialsTeacher ';
+// expected output: Hello World from TutorialsTeacher
 
 // Template literals (Template strings)
 
 const str6 = `string text`;
+// expected output: string text
+
 
 const str7 = `string text line 1
  string text line 2`;
+// expected output: string text line 1
+//                   string text line 2
+
 
 const str8 = `string text ${str6} string text`;
+// expected output: string text string text string text
 
 // Include quotation marks inside string
 
@@ -40,28 +60,44 @@ var lorem2 = 'This is "simple" string';
 // length of string value 
 
 const hello = 'Hello';
-hello.length // 5
+
+hello.length
+// expected output: 5 
 
 // get value of position in string value
-hello.charAt(0); // H
+hello.charAt(0);
+// expected output: H
 
 
 // get unicode of value in string value
-hello.charCodeAt(0) // 72
+hello.charCodeAt(0)
+// expected output: 72
 
 // Joins specified string literal values
 const world = ' world!'; 
-hello.concat(world) // Hello world!
+
+hello.concat(world)
+// expected output: Hello world!
 
 // Returns the index of first occurrence of specified String starting return -1 if not found
-hello.indexOf('l') // 2
-hello.indexOf('l',3) // 3
-hello.indexOf('l',4) // -1 Not found
+hello.indexOf('l')
+// expected output: 2
+
+hello.indexOf('l',3)
+// expected output: 3
+
+hello.indexOf('l',4)
+// expected output: -1 Not found
 
 // Returns the last occurrence index of specified SearchString, starting from specified position. Returns -1 if not found.
-hello.lastIndexOf('l') // 3
-hello.lastIndexOf('l',2) // 2
-hello.lastIndexOf('l',1) // -1 Not found
+hello.lastIndexOf('l')
+// expected output: 3
+
+hello.lastIndexOf('l',2)
+// expected output: 2
+
+hello.lastIndexOf('l',1)
+// expected output: -1 Not found
 
 // 	Compares two strings in the current locale.
 const string1 = 'ก'
@@ -87,5 +123,13 @@ const sortedList = sortThaiDictionary(list)
 
 // Match Search a string for a match using specified regular expression. Returns a matching array.
 hello.match('\H') //[ 'H', index: 0, input: 'Hello', groups: undefined ]
+
+// Search specified string value and replace with specified replace Value string and return new string. Regular expression can also be used as searchValue.
+hello.replace('\l','L') // HeLlo
+hello.replace(/l/g,'L') // HeLLo
+
+// 	Search for a match based on specified regular expression.
+hello.search(/l/) // 2
+hello.search(/h/) // -1 Not found
 
 // ---------------------------------------------------------------
